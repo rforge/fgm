880976391c68fea990d2fb3e87f19707dbe4a745
reinder <reinder@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-02-18 12:13:51 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fgm@43 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/fgpt/R/plot.fg.R
+++ pkg/fgpt/R/plot.fg.R
@@ -1,7 +1,7 @@
 plot.fg <-
-function(fg, plane=0, plot.zero=TRUE, sign.level=0.05){
+function(x, plane=0, plot.zero=TRUE, sign.level=0.05){
   
-  if (class(fg)!="fg") {stop("Object needs to be of class \"fg\"")}
+  if (class(x)!="fg") {stop("Object needs to be of class \"fg\"")}
   if (plane!=0 & plane!=1 & plane!=2) {stop("plane needs to be either 0,1 or 2")}
   if (!is.logical(plot.zero)) {stop("plot.zero needs to be logical")}
   
@@ -14,96 +14,96 @@ function(fg, plane=0, plot.zero=TRUE, sign.level=0.05){
   par.ask <- par("ask")
  
   if(plot.zero==TRUE){
-    xlims <- range(c(0,fg$scales[-length(fg$scales)]))
+    xlims <- range(c(0,x$scales[-length(x$scales)]))
   }else{
-    xlims <- range(fg$scales)
+    xlims <- range(x$scales)
   }
   
-  if(fg$pairwise==FALSE){
+  if(x$pairwise==FALSE){
     
-    pvals <- sapply(fg$c.list,p.val,obsv=fg$c.list[[1]][1])
+    pvals <- sapply(x$c.list,p.val,obsv=x$c.list[[1]][1])
     cols <- ifelse(pvals<=sign.level,"blue","black")
 
     par(mfrow=c(1,1))
     par(fig=c(0,0.9,0,1))
     par(mar=c(5,5,2,0))
-    ylims <- range(fg$c.list[[1]][1]-unlist(fg$c.list))
-    plot(fg$scales,fg$c.list[[1]][1]-sapply(fg$c.list,mean,na.rm = TRUE), ylim=ylims, xlim=xlims, pch=16, col=cols[-length(fg$scales)], xlab="grid cell size", ylab="unexplained Moran's I")
-    segments(fg$scales,fg$c.list[[1]][1]-sapply(fg$c.list,quantile,probs=0.025,na.rm = TRUE),fg$scales,fg$c.list[[1]][1]-sapply(fg$c.list,quantile,probs=0.975,na.rm = TRUE), col=cols[-length(fg$scales)])
+    ylims <- range(x$c.list[[1]][1]-unlist(x$c.list))
+    plot(x$scales,x$c.list[[1]][1]-sapply(x$c.list,mean,na.rm = TRUE), ylim=ylims, xlim=xlims, pch=16, col=cols[-length(x$scales)], xlab="grid cell size", ylab="unexplained Moran's I")
+    segments(x$scales,x$c.list[[1]][1]-sapply(x$c.list,quantile,probs=0.025,na.rm = TRUE),x$scales,x$c.list[[1]][1]-sapply(x$c.list,quantile,probs=0.975,na.rm = TRUE), col=cols[-length(x$scales)])
     abline(h=0, lwd=2, col="red")
     
     par(fig=c(0.9,1,0,1),new=TRUE)
     par(mar=c(5,0.2,2,2))
-    plot(0,fg$c.list[[1]][1]-sapply(fg$c.list,mean,na.rm = TRUE)[length(fg$scales)], ylim=ylims, pch=16, col=cols[length(fg$scales)], xlab="", ylab="",xaxt="n", yaxt="n")
+    plot(0,x$c.list[[1]][1]-sapply(x$c.list,mean,na.rm = TRUE)[length(x$scales)], ylim=ylims, pch=16, col=cols[length(x$scales)], xlab="", ylab="",xaxt="n", yaxt="n")
     axis(1, at=0, labels=quote(infinity), cex.axis=1.5)
-    segments(0,fg$c.list[[1]][1]-sapply(fg$c.list,quantile,probs=0.025,na.rm = TRUE)[length(fg$scales)],0,fg$c.list[[1]][1]-sapply(fg$c.list,quantile,probs=0.975,na.rm = TRUE)[length(fg$scales)],col=cols[length(fg$scales)])
+    segments(0,x$c.list[[1]][1]-sapply(x$c.list,quantile,probs=0.025,na.rm = TRUE)[length(x$scales)],0,x$c.list[[1]][1]-sapply(x$c.list,quantile,probs=0.975,na.rm = TRUE)[length(x$scales)],col=cols[length(x$scales)])
     abline(h=0, lwd=2, col="red")
     
-  }else if (fg$correlate==FALSE){
+  }else if (x$correlate==FALSE){
     if(plane %in% c(0,1)){
       
-      pvals <- sapply(fg$m.list,p.val,obsv=fg$m.list[[1]][1])
+      pvals <- sapply(x$m.list,p.val,obsv=x$m.list[[1]][1])
       cols <- ifelse(pvals<=sign.level,"blue","black")
       
       par(mfrow=c(1,1))
       par(fig=c(0,0.9,0,1))
       par(mar=c(5,5,2,0))
-      ylims <- range(unlist(fg$m.list))
-      plot(fg$scales,sapply(fg$m.list,mean,na.rm = TRUE), ylim=ylims, xlim=xlims, pch=16, col=cols[-length(fg$scales)], xlab="grid cell size", ylab="mean")
-      segments(fg$scales,sapply(fg$m.list,quantile,probs=0.025,na.rm = TRUE),fg$scales,sapply(fg$m.list,quantile,probs=0.975,na.rm = TRUE), col=cols[-length(fg$scales)])
-      abline(h=fg$m.list[[1]][1], lwd=2, col="red")
+      ylims <- range(unlist(x$m.list))
+      plot(x$scales,sapply(x$m.list,mean,na.rm = TRUE), ylim=ylims, xlim=xlims, pch=16, col=cols[-length(x$scales)], xlab="grid cell size", ylab="mean")
+      segments(x$scales,sapply(x$m.list,quantile,probs=0.025,na.rm = TRUE),x$scales,sapply(x$m.list,quantile,probs=0.975,na.rm = TRUE), col=cols[-length(x$scales)])
+      abline(h=x$m.list[[1]][1], lwd=2, col="red")
     
       par(fig=c(0.9,1,0,1),new=TRUE)
       par(mar=c(5,0.2,2,2))
-      plot(0,sapply(fg$m.list,mean,na.rm = TRUE)[length(fg$scales)], ylim=ylims, pch=16, xlab="", ylab="",xaxt="n", yaxt="n",col=cols[length(fg$scales)])
+      plot(0,sapply(x$m.list,mean,na.rm = TRUE)[length(x$scales)], ylim=ylims, pch=16, xlab="", ylab="",xaxt="n", yaxt="n",col=cols[length(x$scales)])
       axis(1, at=0, labels=quote(infinity), cex.axis=1.5)
-      segments(0,sapply(fg$m.list,quantile,probs=0.025,na.rm = TRUE)[length(fg$scales)],0,sapply(fg$m.list,quantile,probs=0.975,na.rm = TRUE)[length(fg$scales)],col=cols[length(fg$scales)])
-      abline(h=fg$m.list[[1]][1], lwd=2, col="red")
+      segments(0,sapply(x$m.list,quantile,probs=0.025,na.rm = TRUE)[length(x$scales)],0,sapply(x$m.list,quantile,probs=0.975,na.rm = TRUE)[length(x$scales)],col=cols[length(x$scales)])
+      abline(h=x$m.list[[1]][1], lwd=2, col="red")
       }
   
     if(plane==0){par(ask=TRUE)}
   
     if(plane %in% c(0,2)){
       
-      pvals <- sapply(fg$v.list,p.val,obsv=fg$v.list[[1]][1])
+      pvals <- sapply(x$v.list,p.val,obsv=x$v.list[[1]][1])
       cols <- ifelse(pvals<=sign.level,"blue","black")
       
       par(mfrow=c(1,1))
       par(fig=c(0,0.9,0,1))
       par(mar=c(5,5,2,0))
-      ylims <- range(unlist(fg$v.list))
-      plot(fg$scales,sapply(fg$v.list,mean,na.rm = TRUE), ylim=ylims, xlim=xlims, pch=16, col=cols[-length(fg$scales)], xlab="grid cell size", ylab="variance")
-      segments(fg$scales,sapply(fg$v.list,quantile,probs=0.025,na.rm = TRUE),fg$scales,sapply(fg$v.list,quantile,probs=0.975,na.rm = TRUE), col=cols[-length(fg$scales)])
-      abline(h=fg$v.list[[1]][1], lwd=2, col="red")
+      ylims <- range(unlist(x$v.list))
+      plot(x$scales,sapply(x$v.list,mean,na.rm = TRUE), ylim=ylims, xlim=xlims, pch=16, col=cols[-length(x$scales)], xlab="grid cell size", ylab="variance")
+      segments(x$scales,sapply(x$v.list,quantile,probs=0.025,na.rm = TRUE),x$scales,sapply(x$v.list,quantile,probs=0.975,na.rm = TRUE), col=cols[-length(x$scales)])
+      abline(h=x$v.list[[1]][1], lwd=2, col="red")
     
       par(fig=c(0.9,1,0,1),new=TRUE)
       par(mar=c(5,0.2,2,2))
-      plot(0,sapply(fg$v.list,mean,na.rm = TRUE)[length(fg$scales)], ylim=ylims, pch=16, xlab="", ylab="",xaxt="n", yaxt="n", col=cols[length(fg$scales)])
+      plot(0,sapply(x$v.list,mean,na.rm = TRUE)[length(x$scales)], ylim=ylims, pch=16, xlab="", ylab="",xaxt="n", yaxt="n", col=cols[length(x$scales)])
       axis(1, at=0, labels=quote(infinity), cex.axis=1.5)
-      segments(0,sapply(fg$v.list,quantile,probs=0.025,na.rm = TRUE)[length(fg$scales)],0,sapply(fg$v.list,quantile,probs=0.975,na.rm = TRUE)[length(fg$scales)], col=cols[length(fg$scales)])
-      abline(h=fg$v.list[[1]][1], lwd=2, col="red")
+      segments(0,sapply(x$v.list,quantile,probs=0.025,na.rm = TRUE)[length(x$scales)],0,sapply(x$v.list,quantile,probs=0.975,na.rm = TRUE)[length(x$scales)], col=cols[length(x$scales)])
+      abline(h=x$v.list[[1]][1], lwd=2, col="red")
     }
 
   }else{
     
-    pvals <- sapply(fg$c.list,p.val,obsv=fg$c.list[[1]][1])
+    pvals <- sapply(x$c.list,p.val,obsv=x$c.list[[1]][1])
     cols <- ifelse(pvals<=sign.level,"blue","black")
     
     par(mfrow=c(1,1))
     par(fig=c(0,0.9,0,1))
     par(mar=c(5,5,2,0))
     
-    ylims <- range(fg$c.list[[1]][1]-unlist(fg$c.list))
-    plot(fg$scales,sapply(fg$c.list,mean,na.rm = TRUE), ylim=ylims, xlim=xlims, pch=16, xlab="grid cell size", ylab="correlation", col=cols[-length(fg$scales)])
-    segments(fg$scales,sapply(fg$c.list,quantile,probs=0.025,na.rm = TRUE),fg$scales,sapply(fg$c.list,quantile,probs=0.975,na.rm = TRUE), col=cols[-length(fg$scales)])
-    abline(h=fg$c.list[[1]][1], lwd=2, col="red")
+    ylims <- range(x$c.list[[1]][1]-unlist(x$c.list))
+    plot(x$scales,sapply(x$c.list,mean,na.rm = TRUE), ylim=ylims, xlim=xlims, pch=16, xlab="grid cell size", ylab="correlation", col=cols[-length(x$scales)])
+    segments(x$scales,sapply(x$c.list,quantile,probs=0.025,na.rm = TRUE),x$scales,sapply(x$c.list,quantile,probs=0.975,na.rm = TRUE), col=cols[-length(x$scales)])
+    abline(h=x$c.list[[1]][1], lwd=2, col="red")
     
     par(fig=c(0.9,1,0,1),new=TRUE)
     par(mar=c(5,0.2,2,2))
-    plot(0,sapply(fg$c.list,mean,na.rm = TRUE)[length(fg$scales)], ylim=ylims, pch=16, xlab="", ylab="",xaxt="n", yaxt="n", col=cols[length(fg$scales)])
+    plot(0,sapply(x$c.list,mean,na.rm = TRUE)[length(x$scales)], ylim=ylims, pch=16, xlab="", ylab="",xaxt="n", yaxt="n", col=cols[length(x$scales)])
     axis(1, at=0, labels=quote(infinity), cex.axis=1.5)
-    segments(0,sapply(fg$c.list,quantile,probs=0.025,na.rm = TRUE)[length(fg$scales)],0,sapply(fg$c.list,quantile,probs=0.975,na.rm = TRUE)[length(fg$scales)], col=cols[length(fg$scales)])
-    abline(h=fg$c.list[[1]][1], lwd=2, col="red")
+    segments(0,sapply(x$c.list,quantile,probs=0.025,na.rm = TRUE)[length(x$scales)],0,sapply(x$c.list,quantile,probs=0.975,na.rm = TRUE)[length(x$scales)], col=cols[length(x$scales)])
+    abline(h=x$c.list[[1]][1], lwd=2, col="red")
     }
   par(fig=par.fig)
   par(mar=par.mar)
--- pkg/fgpt/R/summary.fg.R
+++ pkg/fgpt/R/summary.fg.R
@@ -1,7 +1,7 @@
 summary.fg <-
-function(fg, sign.level=0.05){
+function(object, sign.level=0.05){
   
-  if (class(fg)!="fg") {stop("Object needs to be of class \"fg\"")}
+  if (class(object)!="fg") {stop("Object needs to be of class \"fg\"")}
   
   p.val <- function(perm,obsv){
     return(min((sum(perm>=obsv))/(sum(is.na(perm)==FALSE))*2,(sum(perm<=obsv))/(sum(is.na(perm)==FALSE))*2,1))   
@@ -11,22 +11,22 @@ function(fg, sign.level=0.05){
     return(sum(is.na(perm))/length(perm)*100)  
   }
   
-  if(fg$pairwise==FALSE){
-    c.table <- cbind(fg$scales,fg$c.list[[1]][1]-sapply(fg$c.list,mean,na.rm = TRUE),fg$c.list[[1]][1]-sapply(fg$c.list,quantile,probs=0.025,na.rm = TRUE),fg$c.list[[1]][1]-sapply(fg$c.list,quantile,na.rm = TRUE,probs=0.975),sapply(fg$c.list,p.val,obsv=fg$c.list[[1]][1]),sapply(fg$c.list,fail))
+  if(object$pairwise==FALSE){
+    c.table <- cbind(object$scales,object$c.list[[1]][1]-sapply(object$c.list,mean,na.rm = TRUE),object$c.list[[1]][1]-sapply(object$c.list,quantile,probs=0.025,na.rm = TRUE),object$c.list[[1]][1]-sapply(object$c.list,quantile,na.rm = TRUE,probs=0.975),sapply(object$c.list,p.val,obsv=object$c.list[[1]][1]),sapply(object$c.list,fail))
     c.table <- cbind(c.table,c.table[,5]<=sign.level)
     colnames(c.table) <- c("cell_size","mean","lower_limit", "upper_limit", "P_value", "% failures","Reject H0")
     rownames(c.table) <- rep("",dim(c.table)[1])
-  }else if(fg$correlate==FALSE & fg$pairwise==TRUE){
-    m.table <- cbind(fg$scales,fg$m.list[[1]][1],sapply(fg$m.list,mean,na.rm = TRUE),sapply(fg$m.list,quantile,probs=0.025,na.rm = TRUE),sapply(fg$m.list,quantile,na.rm = TRUE,probs=0.975),sapply(fg$m.list,p.val,obsv=fg$m.list[[1]][1]),sapply(fg$m.list,fail))
+  }else if(object$correlate==FALSE & object$pairwise==TRUE){
+    m.table <- cbind(object$scales,object$m.list[[1]][1],sapply(object$m.list,mean,na.rm = TRUE),sapply(object$m.list,quantile,probs=0.025,na.rm = TRUE),sapply(object$m.list,quantile,na.rm = TRUE,probs=0.975),sapply(object$m.list,p.val,obsv=object$m.list[[1]][1]),sapply(object$m.list,fail))
     m.table <- cbind(m.table,m.table[,6]<=sign.level)  
     colnames(m.table) <- c("cell_size","observed","mean","lower_limit", "upper_limit", "P_value", "% failures","Reject H0")
     rownames(m.table) <- rep("",dim(m.table)[1])
-    v.table <- cbind(fg$scales,fg$v.list[[1]][1],sapply(fg$v.list,mean,na.rm = TRUE),sapply(fg$v.list,quantile,probs=0.025,na.rm = TRUE),sapply(fg$v.list,quantile,na.rm = TRUE,probs=0.975),sapply(fg$v.list,p.val,obsv=fg$v.list[[1]][1]),sapply(fg$v.list,fail))
+    v.table <- cbind(object$scales,object$v.list[[1]][1],sapply(object$v.list,mean,na.rm = TRUE),sapply(object$v.list,quantile,probs=0.025,na.rm = TRUE),sapply(object$v.list,quantile,na.rm = TRUE,probs=0.975),sapply(object$v.list,p.val,obsv=object$v.list[[1]][1]),sapply(object$v.list,fail))
     v.table <- cbind(v.table,v.table[,6]<=sign.level)
     colnames(v.table) <- c("cell_size","observed","mean","lower_limit", "upper_limit", "P_value", "% failures","Reject H0")
     rownames(v.table) <- rep("",dim(v.table)[1])
   }else{
-    c.table <- cbind(fg$scales,fg$c.list[[1]][1],sapply(fg$c.list,mean,na.rm = TRUE),sapply(fg$c.list,quantile,probs=0.025,na.rm = TRUE),sapply(fg$c.list,quantile,na.rm = TRUE,probs=0.975),sapply(fg$c.list,p.val,obsv=fg$c.list[[1]][1]),sapply(fg$c.list,fail))
+    c.table <- cbind(object$scales,object$c.list[[1]][1],sapply(object$c.list,mean,na.rm = TRUE),sapply(object$c.list,quantile,probs=0.025,na.rm = TRUE),sapply(object$c.list,quantile,na.rm = TRUE,probs=0.975),sapply(object$c.list,p.val,obsv=object$c.list[[1]][1]),sapply(object$c.list,fail))
     c.table <- cbind(c.table,c.table[,6]<=sign.level)
     colnames(c.table) <- c("cell_size","observed","mean","lower_limit", "upper_limit", "P_value", "% failures","Reject H0")
     rownames(c.table) <- rep("",dim(c.table)[1])
@@ -37,9 +37,9 @@ function(fg, sign.level=0.05){
   cat(" Floating Grid Permutation Technique","\n")
   cat("=====================================","\n")
   cat("\n")
-  cat(paste("Number of scales:",length(fg$scales)),"\n")
-  cat(paste("Number of iterations per scale:",fg$iter),"\n")
-  if(fg$pairwise==FALSE){
+  cat(paste("Number of scales:",length(object$scales)),"\n")
+  cat(paste("Number of iterations per scale:",object$iter),"\n")
+  if(object$pairwise==FALSE){
     cat("\n")
     cat("---------------------------------------------------------","\n")
     cat(" Results for univariate analyses, unexplained Moran's I:","\n")
@@ -49,7 +49,7 @@ function(fg, sign.level=0.05){
     cat("----------------------------------------------------------------------------","\n")
     cat(paste(" NOTE: Rejection of H0 based on alpha =",sign.level),"\n")
     cat("\n")
-  }else if(fg$correlate==FALSE){
+  }else if(object$correlate==FALSE){
     cat("\n")
     cat("------------------------------------------","\n")
     cat(" Results for multivariate analyses, mean:","\n")
@@ -67,7 +67,7 @@ function(fg, sign.level=0.05){
   }else{
     cat("\n")
     cat("-----------------------------------","\n")
-    cat(" Results for multivariate analyses,",fg$correlate,"correlation:","\n")
+    cat(" Results for multivariate analyses,",object$correlate,"correlation:","\n")
     cat("-----------------------------------","\n")
     cat("\n")
     print(c.table, row.names=FALSE) 
